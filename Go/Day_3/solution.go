f328cd1276140adcd5bd1e8f6fe7795587dc9c98
ziting <ziting.lee@hotmail.com>
2026-06-14 15:49:50 +0800

    day 3 part 2 testing

--- Go/Day_3/solution.go
+++ Go/Day_3/solution.go
@@ -5,9 +5,73 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-func main() {
+func largestSubsequence(s string, length int) string {
+	var stack []rune
+	
+	for i, char := range s {
+		// Pop smaller elements from the stack if we have enough characters 
+		// remaining in the original string to still reach our target length
+		for len(stack) > 0 && stack[len(stack)-1] < char && (len(stack)-1+(len(s)-i)) >= length {
+			stack = stack[:len(stack)-1]
+		}
+		
+		// Push the current character if the stack hasn't reached the target length
+		if len(stack) < length {
+			stack = append(stack, char)
+		}
+	}
+	
+	// Convert the slice of runes back into a string
+	var sb strings.Builder
+	for _, char := range stack {
+		sb.WriteRune(char)
+	}
+	return sb.String()
+}
+
+func part2() int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error reading input file")
+	}
+	scanner := bufio.NewScanner(file)
+	result := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		runes := []rune(line)
+
+		largestVal := runes[:12]
+		i := 1
+		for i < len(runes) {
+			currentVal := int(runes[i] - '0')
+			// appendToLast := false
+			for j := 0; j < len(largestVal); j++ {
+				digit := int(largestVal[j] - '0')
+
+				if currentVal > digit {
+					remaining := len(largestVal) - j
+					if remaining <= len(runes)-i {
+						largestVal = append(largestVal[:j], runes[i:i+remaining]...)
+						break
+					}
+				}
+			}
+			i++
+		}
+		largestInt, err := strconv.Atoi(string(largestVal))
+		if err != nil {
+			fmt.Println("error converting char to string")
+		}
+		//fmt.Printf("largest int at line: %d\n", largestInt)
+		result += largestInt
+	}
+	return result
+}
+
+func part1() {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println("Error reading input file")
@@ -33,12 +97,30 @@ func main() {
 				largestVal[1] = runes[i]
 			}
 		}
-		largestInt,err := strconv.Atoi(string(largestVal))
+		largestInt, err := strconv.Atoi(string(largestVal))
 		if err != nil {
 			fmt.Println("error converting char to string")
 		}
-		fmt.Printf("largest int val: %d\n", largestInt)
 		resultSlice += largestInt
 	}
 	fmt.Println(resultSlice)
+}
+
+func main() {
+	fmt.Println(part2())
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error reading input file")
+	}
+	scanner := bufio.NewScanner(file)
+	result := 0
+	for scanner.Scan() {
+		text := scanner.Text()
+		val, err := strconv.Atoi(largestSubsequence(text,12))
+		if err != nil {
+			fmt.Println("error converting char to string")
+		}
+		result += val 
+	}
+	fmt.Println(result)
 }
